afaa682e85952eb0127e3bd907fcf9a1b9529f9e
Samuel Stjernstrom <samuel.stjenrnstrom@omega365.com>
2024-06-19 11:24:25 +0200

    added ts prop defining

--- components/DefineProps.vue
+++ components/DefineProps.vue
@@ -17,11 +17,14 @@ const props = defineProps({
 // Typescript way
 <script setup lang="ts">
 
+import defineProps from "./DefineProps.vue";
+
 interface Props { // can be in separate file ex if the type is a big object
     foo: string
     bar?: number
 }
 
-const props = defineProps<Props>()
+const props: Props = defineProps()
+
 
 </script>
